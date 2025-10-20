3021038b7987823b973c149ee40f39bae0f5355f
Olivers <bindris.olivers@gmail.com>
2025-10-20 08:52:18 +0300

    5.uzd

---
+++ array-and-for-loop.js
@@ -0,0 +1,7 @@
+const fruit = [];
+fruit.push("banana","apple","mango","watermelon");
+console.log(fruit.length);
+console.log("banana");
+console.log("apple");
+console.log("mango");
+console.log("watermelon")
