fbd211e778d8f725456444fd67d8b01f4ded42fc
Olivers <bindris.olivers@gmail.com>
2025-10-20 09:00:24 +0300

    7.uzd

---
+++ function.js
@@ -0,0 +1,19 @@
+
+let str = "";
+for (let i = 5 ; i < 9; i++) {
+  str += i;
+}
+
+console.log(str);
+let str1 = "";
+for (let i = 1 ; i < 11; i++) {
+  str1 += i;
+}
+
+console.log(str1);
+let str2 = "";
+for (let i = 7 ; i < 17; i++) {
+  str2 += i;
+}
+
+console.log(str2);
